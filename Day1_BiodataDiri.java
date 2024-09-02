9743dd34eb05a0a6d5a58b47efd9e994a366b7c8
Hasnur112 <kainbekas02@gmail.com>
2024-09-02 17:54:43 +0800

    Add files via upload

---
+++ Day1_BiodataDiri.java
@@ -0,0 +1,24 @@
+package hasnur;
+
+public class Hasnur {
+
+    public static void main(String[] args) {
+        System.out.println("Biodata saya");
+        String n = "HASNUR";
+        String NIM = "D0223509";
+        char g = 'L';
+        char k = 'E';
+        byte u = 20;
+        String a = "2023";
+        String al = "Ugi Baru \n\t\t Kec.mapilli";
+
+        System.out.println("Nama \t\t:" + n);
+        System.out.println("NIM \t\t:" + NIM);
+        System.out.println("Kelas \t\t:" + k);
+        System.out.println("Angkatan \t:" + a);
+        System.out.println("Umur \t\t:" + u);
+        System.out.println("Alamat \t\t:" + al);
+
+    }
+
+}
