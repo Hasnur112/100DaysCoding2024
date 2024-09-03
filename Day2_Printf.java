8f2ff193402b23f12a389a921466100611062fac
Hasnur112 <kainbekas02@gmail.com>
2024-09-03 11:55:58 +0800

    Create Day2_Printf.java
    Hari ke 2 belajar menggunakan printf

---
+++ Day2_Printf.java
@@ -0,0 +1,11 @@
+package Java;
+public class Day2 {
+    public static void main(String[] args) {
+        int d = 2;
+        String c ="Printf";
+        int b = 100;
+        float j = -99.5f;
+        //printf dimaksud untuk mencetak format string yang lebih flexibel dan mendukung format khusus
+        System.out.printf("\nHari ke-:%d\nMateri:%s\nJumlah Hari:%d\nKeadaan ekonomi:%.1f",d,c,b,j);
+    }
+}
