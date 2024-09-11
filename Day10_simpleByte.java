4a3b30be9fbc68a358eeb25e75ff04cd3e5867ff
Hasnur112 <kainbekas02@gmail.com>
2024-09-11 17:40:56 +0800

    Create Day10_simpleByte.java

---
+++ Day10_simpleByte.java
@@ -0,0 +1,25 @@
+import java.util.*;
+ class Day10 {
+   //main method
+   public static void main(String args[])
+   { 
+    System.out.println("Day10 byte");
+    //variabel deklarasi
+    byte b1,b2,b3;
+    //variabel inisialisasi
+    b1 = 10;
+    b2 = 15;
+    b3 = 27;
+    //printf
+    System.out.printf("\n%d \n%d \n%d\n",b1,b2,b3);
+    //println
+    System.out.println("Total:"+b1+b2+b3);
+    //variabel update 
+    b1 = 9;
+    b2 = 13;
+    b3 = 25;
+    System.out.println("nilai Setelah update");
+    System.out.printf("%d \n%d \n%d\n",b1,b2,b3);
+    System.out.println("Total"+b1+b2+b3);
+   }
+ }
