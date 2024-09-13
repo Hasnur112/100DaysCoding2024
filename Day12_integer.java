c68eb576ffa141170d4422c64c101a33c23ebdc4
Hasnur112 <kainbekas02@gmail.com>
2024-09-13 18:45:57 +0800

    Create Day12_integer.java
    belajar integer dan final variabel

---
+++ Day12_integer.java
@@ -0,0 +1,23 @@
+class Day12 {
+   public static void main(String args[]) { 
+    System.out.println("Day12_integer");
+    //final varibel
+    final String nama = "HASNUR";
+    final String nim = "D0223509";
+    //deklarasi
+    int a,b,c,d;
+    //inisialisasi
+    a = 102;
+    b = 204;
+    c = 306;
+    d = 408;
+    //keluaran atau print dengan format
+    System.out.printf("\nNilai data pertama: %d\nNilai data ke-dua: %d\nNilai data ke-tiga: %d\nNilai data ke-empat: %d\n",a,b,c,d);
+    //update (mengubah nilai variabel kita)
+    a = 1025;
+    b = 90;
+    //print variabel yang sudah kita update
+    System.out.println("Data update");
+    System.out.printf("Data pertama: %d\nData ke-Dua: %d",a,b);
+   }
+ }
