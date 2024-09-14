5e12ac0ebcbe2316fe6bef686f84f955923fa78b
Hasnur112 <kainbekas02@gmail.com>
2024-09-14 20:52:12 +0800

    Create day13_tipedata.java

---
+++ day13_tipedata.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ class Day13 {
+   public static void main(String args[]) {       
+        System.out.println("Day 13 welcome");
+        String nama = "Hasnur";
+        System.out.println("Nama: " + nama);
+        byte umur = 20;
+        System.out.println("Umur: " + umur);
+        short  nilai = 85;
+        System.out.println("Nilai percaya diri: " + nilai);
+        int charisma = -999999;
+        System.out.println("Jumlah charisma: " + charisma);
+        long jarak = 1000000000;
+        System.out.println("Jarak ke-kampus: " + jarak+"M");
+        float tinggi = 173.5f;
+        System.out.println("Tinggi: " + tinggi);
+        double berat = 69.7;
+        System.out.println("Berat: " + berat);
+        char jenisKelamin = 'L';
+        System.out.println("Jenis Kelamin: " + jenisKelamin);
+        boolean mahasiswa = true;
+        System.out.println("Mahasiswa? = "+mahasiswa);
+    }
+}
