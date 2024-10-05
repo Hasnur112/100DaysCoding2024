8289ec95433de17bcee9e906c11ff046058606cd
Hasnur112 <kainbekas02@gmail.com>
2024-10-05 23:12:22 +0800

    Create Day34_OperatorAritmatika.java

---
+++ Day34_OperatorAritmatika.java
@@ -0,0 +1,26 @@
+public class Day34
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("Operator aritmatika");
+    int a,b,c,d,e,f;
+    a = 35;
+    b = 25;
+    c = 29;
+    d = 60;
+    e = 55;
+    f = 10;
+    
+    //operator 
+    int na,nb,nc,nd,ne;
+    na = a + b;
+    nb = d - c;
+    nc = a * f;
+    nd = e % f;
+    ne = d / f;
+    
+    //menampilkan
+    System.out.printf ("a + b = %d\nd - c = %d\na × f = %d\ne mod f = %d\nd / f = %d\n",na,nb,nc,nd,ne);
+    
+   }
+ }
