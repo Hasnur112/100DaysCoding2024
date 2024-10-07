f176697be0d97b607f12d22355ea26ea9c8955d7
Hasnur112 <kainbekas02@gmail.com>
2024-10-07 15:55:59 +0800

    Create Day36_opAritmatika.java

---
+++ Day36_opAritmatika.java
@@ -0,0 +1,20 @@
+public class Day36
+ {
+   public static void main(String args[])
+   { 
+    System.out.println("operator aritmatika");
+    int a,b,c,d;
+    a = 20;
+    b = 40;
+    c = 80;
+    d = 35;
+    //perkalian modulo dan pembagian
+    int aa,bb,cc;
+    aa = a * b; //perkalian
+    bb = c / b; //pembagian
+    cc = d % 4; //modulo
+    System.out.printf("Hasil perkalian dari 20 × 40 = %d\n",aa);
+    System.out.printf("Hasil pembagian dari 80 / 40 = %d\n ",bb);
+    System.out.println("Hasil modulo atau sisa bagi dari 35 % 4 = "+cc);
+   }
+ }
