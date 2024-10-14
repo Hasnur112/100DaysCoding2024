d745d83aef6cb3c08cb1572aa3c1517cf75ff34b
Hasnur112 <kainbekas02@gmail.com>
2024-10-14 16:04:57 +0800

    Create Evaluasi(2)soal2.java

---
+++ Evaluasi(2)soal2.java
@@ -0,0 +1,17 @@
+public class soal2 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 20;
+        System.out.println(b);
+        b = 5;
+        System.out.println(b);
+        b = a * b;
+        System.out.println(b);
+        b -= a;
+        System.out.println(b);
+        b = b / 15;
+        System.out.println(b);
+        b = a + 4;
+        System.out.println(b);
+    }
+}
