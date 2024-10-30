8b781dd2ff71f91e2d44e42e60125a569a01cffa
Hasnur112 <kainbekas02@gmail.com>
2024-10-30 21:34:50 +0800

    Create Day59_forEach.java

---
+++ Day59_forEach.java
@@ -0,0 +1,10 @@
+public class Day59 {
+    public static void main(String[] args) {
+        double a = 1927.77888; 
+        String b = "" + a; 
+      
+        for (char i : b.toCharArray()) {
+            System.out.println("angka: " + i);
+        }
+    }
+}
