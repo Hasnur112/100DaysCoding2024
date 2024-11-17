5dab30104d0f381c86653d1a81339724277e1757
Hasnur112 <kainbekas02@gmail.com>
2024-11-17 22:53:18 +0800

    Create Day77_equals.java

---
+++ Day77_equals.java
@@ -0,0 +1,17 @@
+import  java.util.Scanner;
+public class day77 {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Masukkan film favorit : ");
+        String  f = ip.nextLine();
+        
+        if(f.equalsIgnoreCase("Iron Man")){
+            System.out.println("kita memiliki selara yang sama");
+        }else if(f.equalsIgnoreCase(f)){
+            System.out.println(f+" memang bagus tapi saya lebih suka Iron Man");
+        }else{
+            System.out.println("String tdk valid");
+        }
+        ip.close();
+    }
+}
