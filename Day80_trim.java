6439ac08d6a56e3372a9626938007810bf32562e
Hasnur112 <kainbekas02@gmail.com>
2024-11-20 21:08:12 +0800

    Create Day80_trim.java

---
+++ Day80_trim.java
@@ -0,0 +1,9 @@
+public class day80 {
+    public static void main(String[] args) {
+        String a = "           pangkas rambut         ";
+        System.out.printf("Sebelum trim: '%s'%n", a);
+
+        String b = a.trim();
+        System.out.printf("Setelah trim: '%s'%n", b);
+    }
+}
