f32e5f7a549fa5241ad0f055637f30c09da54332
Hasnur112 <kainbekas02@gmail.com>
2024-11-23 22:19:09 +0800

    Create Day83_deklarasiArray.java

---
+++ Day83_deklarasiArray.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class day83{
+    public static void main(String[] args){
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Masukkan panjang array: ");
+        int panjang = ip.nextInt();
+        int a [] ;//deklarasi
+        
+        a = new int[panjang];//mengisi parameter array dengan panjang yang diinginkan pennguna
+        
+        ip.close();
+    }
+}
