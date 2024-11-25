cb2beaf35c6618b40bcf7ba075b7e8b237b3ea4c
Hasnur112 <kainbekas02@gmail.com>
2024-11-25 22:19:44 +0800

    Create Day85_arrayInisialisasi.java

---
+++ Day85_arrayInisialisasi.java
@@ -0,0 +1,9 @@
+public class day85 {
+    public static void main(String[] args) {
+        int a [] = {1, 2, 7, 9,5,5};
+            
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Elemen no- " + i + " : " + a[i]);
+        }
+    }
+}
