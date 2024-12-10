e3f1944d61f6645dc4fbd9e490018227076077d2
Hasnur112 <kainbekas02@gmail.com>
2024-12-10 20:45:00 +0800

    Create Day100.java

---
+++ Day100.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.time.LocalTime;
+
+public class day100 {
+    public static void main(String[] args) {
+        Scanner ip = new Scanner(System.in);
+        
+        System.out.println("masukkan nama: ");
+        String nama = ip.nextLine();
+        LocalTime sekarang = LocalTime.now();
+        String salam;
+
+        if (sekarang.isBefore(LocalTime.now())) {
+            salam = "Selamat pagi";
+        } else if (sekarang.isBefore(LocalTime.of(17, 0))) {
+            salam = "Selamat siang";
+        } else {
+            salam = "Selamat malam";
+        }
+        System.out.println("================================================");
+        System.out.println(salam + ", " + nama);
+        System.out.println("Progam Anda Telak Selesai");
+        System.out.println("================================================");
+    }
+
+}
